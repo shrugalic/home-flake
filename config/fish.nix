2ad953038f625c5a927fa38af118987c1dcd21c9
bOli <github@bueechi.net>
2023-02-17 16:00:37 +0100

    Add alias for new workspace

--- config/fish.nix
+++ config/fish.nix
@@ -51,6 +51,7 @@
       md = "set -g PROJECT mnet && set -g WORKSPACE $WORKSPACES/mnet/devel/ && pj";
       mt = "set -g PROJECT mnet && set -g WORKSPACE $WORKSPACES/mnet/test/ && pj";
       mp = "set -g PROJECT mnet && set -g WORKSPACE $WORKSPACES/mnet/prod/ && pj";
+      mgb = "set -g PROJECT mnet && set -g WORKSPACE $WORKSPACES/mnet/gradle/ && pj";
       wg = "cd ~/work/git_projects";
       gaf = "ws; ../git-all fetch; pj";
       gap = "ws; ../git-all pull; pj";
